eda7818e407edcea03051689c797c2387a60a87f
Abhishek Kumar Mishra <71481119+99002440@users.noreply.github.com>
2020-09-19 12:27:22 +0530

    Update Calc_operation.c

--- Calc_operation.c
+++ Calc_operation.c
@@ -22,3 +22,51 @@ int divide(int operand1, int operand2)
     else
         return operand1 / operand2;
 }
+void modulus()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number  : "); 
+    scanf("%d", &b);
+    d=a%b;
+    printf("\nModulus of entered numbers = %d\n",d);
+}
+
+void power()
+{
+    double a,num, p;
+    printf("\nEnter two numbers to find the power \n");
+    printf("number: ");
+    scanf("%lf",&a);
+
+    printf("power : ");
+    scanf("%lf",&num);
+
+    p=pow(a,num);
+
+    printf("\n%lf to the power %lf = %lf \n",a,num,p);
+}
+
+int factorial()
+{
+    int i,fact=1,num;
+
+    printf("\nEnter a number to find factorial : ");
+    scanf("%d",&num);
+
+    if (num<0)
+    {
+        printf("\nPlease enter a positive number to");
+        printf(" find factorial and try again. \n");
+        printf("\nFactorial can't be found for negative");
+        printf(" values. It can be only positive or 0  \n");
+        return 1;
+    }               
+
+    for(i=1;i<=num;i++)
+    fact=fact*i;
+    printf("\n");
+    printf("Factorial of entered number %d is:%d\n",num,fact);
+    return 0;
+}
