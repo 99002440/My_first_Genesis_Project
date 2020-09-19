c5d384b48d8265e7541bb7e2a85b6963ed18ce29
Abhishek Kumar Mishra <71481119+99002440@users.noreply.github.com>
2020-09-19 13:55:56 +0530

    Update Calc_operation.c

--- Calc_operation.c
+++ Calc_operation.c
@@ -70,3 +70,26 @@ int factorial()
     printf("Factorial of entered number %d is:%d\n",num,fact);
     return 0;
 }
+int convert_b_to_decimal(long long n) {
+    int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+    }
+    return dec;
+}
+//code taken from :-https://www.programiz.com/c-programming/examples/binary-decimal-convert
+long long convert_d_to_b(int n) {  
+    long long bin = 0;
+    int rem, i = 1, step = 1;
+    while (n != 0) {
+        rem = n % 2;
+        printf("Step %d: %d/2, Remainder = %d, Quotient = %d\n", step++, n, rem, n / 2);
+        n /= 2;
+        bin += rem * i;
+        i *= 10;
+    }
+    return bin;
+}
